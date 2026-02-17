6dcf7ab34e45a76c290c92d5809daae61cf15b85
JoaoVitor-Moreira <joaosvmoreira@gmail.com>
2026-02-17 19:35:59 -0300

    Update number limit from 60 to 70

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 60;
+let numeroLimite = 70;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial () {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 60');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 70');
 }
 exibirMensagemInicial()
 
